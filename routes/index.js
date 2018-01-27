4728d649cc3205b7057b9e5e19fe4d526c3062a7
phillipla <lakich@pnw.edu>
2018-01-27 04:57:56 +0000

    create intial routes,views,models

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var router = express.Router();
+var User = require("../models/user"); 
+
+
+// ROOT Route 
+router.get("/", function(req, res){
+   res.render("home"); 
+});
+
+//==================
+//AUTH Routes
+//==================
---
+++ routes/userpages.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var router = express.Router();
+var Userpage = express.require("../models/userpage");
+
+//======================
+//USERPAGES ROUTES
+//======================
+
+//INDEX Route
+
+router.get("/", function(req, res){
+    res.send("You are here!");
+    
+});
+
+//CREATE Route 
+
+router.post("/", function(req, res){
+       res.send("You are here!");
+});
+
+//NEW Route 
+
+router.get("/new", function(req, res) {
+   res.render("userpages/new"); 
+});
+
+
+//SHOW Route 
+
+router.get("/:id", function(req, res) {
+    res.send("You are here!");
+});
