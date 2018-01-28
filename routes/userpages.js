47b3dcd30b0850ba751461d6c9ab986d81353f42
phillipla <lakich@pnw.edu>
2018-01-28 00:30:09 +0000

    finish initial user index, new, and show pages, routes, and views

--- routes/userpages.js
+++ routes/userpages.js
@@ -9,14 +9,43 @@ var Userpage = require("../models/userpage");
 //INDEX Route
 
 router.get("/", function(req, res){
-    res.send("You are here!");
-    
+    Userpage.find({}, function(err, allUserpages){
+       if(err)
+       {
+           console.log(err);
+       }
+       else
+       {
+           res.render("userpages/index", {userpages: allUserpages, page: "userpages" }); //get rid of currentuser later and put in app.js
+       }
+    });
 });
 
 //CREATE Route 
 
 router.post("/", function(req, res){
-       res.send("You are here!");
+   var name = req.body.name;
+   var image = req.body.image;
+   var description = req.body.description;
+   /*var author = {
+       id: req.user._id,
+       username: req.user.username
+   }*/
+   
+       var newUserpage = {name: name, image: image, description: description};
+   
+       // Create a new campground and save to DB
+       Userpage.create(newUserpage, function(err, newlyCreated){
+           if(err)
+           {
+               console.log(err);
+           }
+           else
+           {
+               console.log(newlyCreated);
+               res.redirect("/userpages"); 
+           }
+        });
 });
 
 //NEW Route 
@@ -29,7 +58,17 @@ router.get("/new", function(req, res) {
 //SHOW Route 
 
 router.get("/:id", function(req, res) {
-    res.send("You are here!");
+    Userpage.findById(req.params.id, function(err, foundUserpage){
+       if(err || !foundUserpage)
+       {
+           console.log(err);
+           res.redirect("back");
+       }
+       else
+       {
+           res.render("userpages/show", {userpage:foundUserpage});
+       }
+    });
 });
 
 module.exports = router;
