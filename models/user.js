613bcc45e6fcdc99d60044896a8272f1b9d02a4f
phillipla <lakich@pnw.edu>
2018-02-02 22:06:00 +0000

    define user model

--- models/user.js
+++ models/user.js
@@ -1,11 +1,14 @@
 var mongoose = require("mongoose");
-//var passportLocalMongoose = require("passport-local-mongoose");
+var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
    username: String,
-   password: String
+   password: String,
+   isAdmin: false,
+   hasProfile: false
 });
 
-//UserSchema.plugin(passportLocalMongoose); //adds important methods and functionality to User model
+UserSchema.plugin(passportLocalMongoose); //for authenticate, serialize and deserialze methods 
+                                          //needed for passport. 
 
 module.exports= mongoose.model("User", UserSchema); 
