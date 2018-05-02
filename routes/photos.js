3982e48bb0da3194903423ccb86f59d299df9505
phillipla <lakich@pnw.edu>
2018-05-02 01:50:11 +0000

    fix comments not populating in photoblog route, added initial modal, added new photo button

--- routes/photos.js
+++ routes/photos.js
@@ -13,7 +13,7 @@ var middleware = require("../middleware");
 router.get("/", function(req, res) 
 {
     //have to use userpage because userpage model contains array of photos (i.e., the photoblog itself)
-    Userpage.findById(req.params.id, function(err, userpage)
+    Userpage.findById(req.params.id).populate("photos").exec(function(err, userpage)
     {
         if(err)
         {
@@ -26,5 +26,59 @@ router.get("/", function(req, res)
     });
     
 });
+
+router.get("/new", function(req, res)
+{
+   Userpage.findById(req.params.id, function(err, userpage)
+   {
+       if(err)
+       {
+           console.log(err);
+       }
+       else
+       {
+           res.render("photos/new", {userpage: userpage});
+       }
+   });
+});
+
+router.post("/", function(req, res)
+{
+    Userpage.findById(req.params.id, function(err, userpage)
+    {
+        if(err || !userpage)
+        {
+            res.redirect("back");
+        }
+        else
+        {
+            Photo.create(req.body.photo, function(err, photo)
+            {
+                if(err)
+                {
+                    console.log(err);
+                }
+                else
+                {
+                     photo.owner.id = userpage._id; //make sure user islogged in before post photo
+                     photo.owner.name = userpage.name;
+                     //save photo
+                     photo.save();
+                     userpage.photos.push(photo._id); //each userpage has photos arrary (see userpage.js) and this pushes new photo from the create into the array
+                     userpage.save();
+    
+                     
+                     console.log(photo);
+                     console.log('\n\n');
+                     
+                     console.log('\n\n Hello');
+                     console.log(userpage);
+                     
+                     res.redirect("/userpages/" + userpage._id + "/photos");
+                }
+            });
+        }
+    });
+})
 
 module.exports = router; 
