49bbe09f50f1c19e7e3d65117565d13571d6a825
phillipla <lakich@pnw.edu>
2018-05-03 19:58:25 +0000

    tweak javascript for photoblog, styles, and  view; finish initial photoblog implementation

--- public/javascript/photoblog.js
+++ public/javascript/photoblog.js
@@ -1,12 +1,14 @@
 
 
-var photo = document.querySelectorAll(".photo");
+var photo = document.querySelectorAll(".photoblogIMG");
 var description = document.querySelectorAll(".photoDescription");
+var caption = document.getElementById("modalCaption");
 var modalPhoto = document.querySelector("#modalPhoto");
 
 for(let i = 0; i < photo.length; i++)
 {
     photo[i].addEventListener("click", function(e) {
-        modalPhoto.innerHTML = this.innerHTML;
+        modalPhoto.src = this.src;
+        caption.innerHTML = description[i].innerHTML;
     })
 }
